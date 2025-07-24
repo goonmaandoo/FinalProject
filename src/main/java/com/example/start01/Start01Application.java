f35b02d02d9b93ea6ff31280f8fce3523f3a9c7f
Gungogoma <sunsh45@naver.com>
2025-07-24 12:57:20 +0900

    gradle, properties 수정

--- src/main/java/com/example/start01/Start01Application.java
+++ src/main/java/com/example/start01/Start01Application.java
@@ -1,9 +1,11 @@
 package com.example.start01;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.start01.dao")
 public class Start01Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/start01/dto/StoreDto.java
+++ src/main/java/com/example/start01/dto/StoreDto.java
@@ -1,6 +1,5 @@
 package com.example.start01.dto;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
