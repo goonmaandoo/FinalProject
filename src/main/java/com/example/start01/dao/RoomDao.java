40075d65b406ee8fbd8fed06779d96377e2a421f
goonmaandoo <won020738@gmail.com>
2025-07-30 18:03:41 +0900

    헤더, 공구방 데이터

---
+++ src/main/java/com/example/start01/dao/RoomDao.java
@@ -0,0 +1,24 @@
+package com.example.start01.dao;
+
+import com.example.start01.dto.RoomDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface RoomDao {
+    List<RoomDto> RoomAll();
+    List<RoomDto> RoomSelectRecruit();
+
+    int RoomInsert(RoomDto room);
+
+    List<RoomDto> RoomsWithJoinCount();
+
+    @Select("SELECT * FROM room WHERE status=#{status}")
+    RoomDto RoomSelect(String status);
+
+
+}
