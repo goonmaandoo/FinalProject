0e8ef7d65786a6b53322e4943b4bc134a2b142c8
samataewoong <ltwng99@naver.com>
2025-07-30 17:57:15 +0900

    qna first done

--- src/main/java/com/example/start01/controller/QnaController.java
+++ src/main/java/com/example/start01/controller/QnaController.java
@@ -3,14 +3,13 @@ package com.example.start01.controller;
 import com.example.start01.dto.QnaDto;
 import com.example.start01.service.QnaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/qna")
-public class MyPageController {
+public class QnaController {
     @Autowired
     private QnaService qnaService;
 
@@ -22,6 +21,21 @@ public class MyPageController {
         dtos.forEach(System.out::println);
         return dtos;
     }
+
+    @GetMapping("/getTheQna/{qnaId}")
+    public QnaDto selectOneQna(@PathVariable Integer qnaId) {
+        System.out.println("qnaId:" + qnaId);
+        QnaDto dto = qnaService.selectByQnaId(qnaId);
+        System.out.println("dto:" + dto);
+        return dto;
+    }
+
+    @PutMapping("/updateQna")
+    public void updateQna(@RequestBody QnaDto qnaDto) {
+        System.out.println(qnaDto);
+        qnaService.updateQna(qnaDto);
+    }
+
     @DeleteMapping("/{qnaId}")
     public void deleteQna(@PathVariable Integer qnaId) {
         System.out.println("삭제 요청: " + qnaId);
