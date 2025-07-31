0828410ef0f6672322beba0e201863ead12a4dc2
kahng <gloomy5480@hotmail.com>
2025-07-31 15:52:16 +0900

    Orders 작업

---
+++ src/main/java/com/example/start01/service/OrdersService.java
@@ -0,0 +1,36 @@
+package com.example.start01.service;
+
+import com.example.start01.dao.OrdersDao;
+import com.example.start01.dto.OrdersDto;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+
+@Service
+public class OrdersService {
+
+    @Autowired
+    private OrdersDao ordersDao;
+
+
+    // insert
+    public void insertOrder(OrdersDto dto) {
+        ordersDao.insertOrder(dto);
+    }
+
+    // selectByUserId
+    public ArrayList<OrdersDto> selectByUserId(Integer userId) {
+        return ordersDao.selectByUserId(userId);
+    }
+
+    // selectByOrderId
+    public OrdersDto selectByOrderId(Integer orderId) {
+        return ordersDao.selectByOrderId(orderId);
+    }
+
+    // delete
+    public void deleteByOrderId(Integer orderId) {
+        ordersDao.deleteByOrderId(orderId);
+    }
+}
