3c12d3f57d95bc282fe33d9c46e5b818c808ed77
goonmaandoo <won020738@gmail.com>
2025-08-19 03:02:12 +0900

    ㄴ3설정 끝

---
+++ src/main/java/com/example/start01/s3/FileUploadController.java
@@ -0,0 +1,20 @@
+package com.example.start01.s3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/files")
+@RequiredArgsConstructor
+public class FileUploadController {
+
+    private final S3Service s3Service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) throws Exception {
+        String url = s3Service.upload(file);
+        return ResponseEntity.ok(url);
+    }
+}
