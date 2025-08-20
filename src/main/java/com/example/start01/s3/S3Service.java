fbebb3cade4be55a1fce748b4bed56a5ecc2a056
goonmaandoo <won020738@gmail.com>
2025-08-21 00:57:28 +0900

    프로필 완료

--- src/main/java/com/example/start01/s3/S3Service.java
+++ src/main/java/com/example/start01/s3/S3Service.java
@@ -18,6 +18,10 @@ public class S3Service {
 
     private final S3Template s3Template;
     private final UsersService usersService;
+<<<<<<< HEAD
+=======
+
+>>>>>>> 2bcbb9f (경로수정)
 
     @Value("${app.s3.bucket}")
     private String bucket;
@@ -63,9 +67,13 @@ public class S3Service {
         URL signed = s3Template.createSignedGetURL(bucket, key, Duration.ofMinutes(10));
         return signed.toString(); // 프론트에서 바로 표시/다운로드 가능 (유효기간 10분)
     }
-
     public String uploadProfile(MultipartFile file, Integer userId) throws Exception {
 
+<<<<<<< HEAD
+    public String uploadProfile(MultipartFile file, Integer userId) throws Exception {
+
+=======
+>>>>>>> 2bcbb9f (경로수정)
         if (file == null || file.isEmpty()) {
             throw new IllegalArgumentException("업로드할 파일이 없습니다.");
         }
@@ -74,7 +82,10 @@ public class S3Service {
         String ext = (original != null && original.contains(".")) ?
                 original.substring(original.lastIndexOf('.')) : "";
 
+<<<<<<< HEAD
         //프로필 이미지 경로
+=======
+>>>>>>> 2bcbb9f (경로수정)
         String key = "profileimg/" + userId + "/profile" + ext;
 
         try (InputStream is = file.getInputStream()) {
@@ -86,11 +97,18 @@ public class S3Service {
                             .contentType(file.getContentType()).build());
         }
 
+<<<<<<< HEAD
+=======
+        // 이제 null이 아니므로 안전하게 호출 가능
+>>>>>>> 2bcbb9f (경로수정)
         usersService.updateProfileUrl(userId, key);
 
         return key;
     }
+<<<<<<< HEAD
 
+=======
+>>>>>>> 2bcbb9f (경로수정)
     public void delete(String key) {
         s3Template.deleteObject(bucket, key);
     }
